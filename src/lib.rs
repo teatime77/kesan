c87e31909331705d145d1c03a80b9b08a325e345
teatime77 <teatime77@live.jp>
2019-10-19 13:09:07 +0900

    console.log

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate wasm_bindgen;
+
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    console::log_1(&format!("Hello, {}!", name).into());
+
+    console::log_1(&"今日はHello using web-sys".into());
+
+    let js: JsValue = 4.into();
+    console::log_2(&"Logging arbitrary values looks like".into(), &js);
 }
