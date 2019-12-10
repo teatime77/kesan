348751a7b33b447e069b713c41ce1e44a4706f5f
teatime77 <teatime77@live.jp>
2019-12-11 08:45:31 +0900

    gpgpu.tsと結合

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,4 @@
+import { CreateGPGPU, Color, Points } from "../../gpgpu.ts/ts/gpgpu.js";
 
 function msg(text: string){
     console.log(text);
@@ -5,4 +6,12 @@ function msg(text: string){
 
 export function initKesan(){
     msg("計算の初期処理");
+
+    var canvas = document.getElementById("webgl-canvas") as HTMLCanvasElement;
+    let mygpgpu = CreateGPGPU(canvas);
+
+    mygpgpu.startDraw3D([ 
+        new Points(new Float32Array([1.5, -1.3, 0, -1.5, -1.3, 0]), Color.red, 5)
+    ]);
+
 }
